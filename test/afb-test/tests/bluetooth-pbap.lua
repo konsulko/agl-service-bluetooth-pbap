6e133e17483d6e6223a758ed94525191d0ac3723
Edi Feschiyan <edi.feschiyan@konsulko.com>
2019-05-02 18:19:28 +0300

    Added a few more tests https://jira.automotivelinux.org/browse/SPEC-2374 affects testing

--- test/afb-test/tests/bluetooth-pbap.lua
+++ test/afb-test/tests/bluetooth-pbap.lua
@@ -16,11 +16,21 @@
  limitations under the License.
 --]]
 
+--[[ A paired phone/device having a phonebook is needed to run the tests
 
-_AFT.testVerbStatusSuccess('testcontactsSuccess','bluetooth-pbap','contacts', {})
-_AFT.testVerbStatusSuccess('testentrySuccess','bluetooth-pbap','entry', {})
-_AFT.testVerbStatusSuccess('testhistorySuccess','bluetooth-pbap','history', {})
-_AFT.testVerbStatusSuccess('testsearchSuccess','bluetooth-pbap','search', {})
-_AFT.testVerbStatusSuccess('teststatusSuccess','bluetooth-pbap','status', {})
-_AFT.testVerbStatusSuccess('testsubscribeSuccess','bluetooth-pbap','subscribe', {})
-_AFT.testVerbStatusSuccess('testunsubscribeSuccess','bluetooth-pbap','unsubscribe', {})
+--]]
+
+_AFT.testVerbStatusSuccess('testContactsSuccess','bluetooth-pbap','contacts', {})
+_AFT.testVerbStatusSuccess('testIncomingCallsEntrySuccess','bluetooth-pbap','entry', {list="ich",handle="1.vcf"})
+_AFT.testVerbStatusSuccess('testOutgoingCallsEntrySuccess','bluetooth-pbap','entry', {list="och",handle="1.vcf"})
+_AFT.testVerbStatusSuccess('testMissedCallsEntrySuccess','bluetooth-pbap','entry', {list="mch",handle="1.vcf"})
+_AFT.testVerbStatusSuccess('testCombinedCallsEntrySuccess','bluetooth-pbap','entry', {list="cch",handle="1.vcf"})
+_AFT.testVerbStatusSuccess('testPhoneBookEntrySuccess','bluetooth-pbap','entry', {list="pb",handle="1.vcf"})
+_AFT.testVerbStatusSuccess('testIncomingCallsHistorySuccess','bluetooth-pbap','history', {list="ich"})
+_AFT.testVerbStatusSuccess('testOutgoingCallsHistorySuccess','bluetooth-pbap','history', {list="och"})
+_AFT.testVerbStatusSuccess('testMissedCallsHistorySuccess','bluetooth-pbap','history', {list="mch"})
+_AFT.testVerbStatusSuccess('testCombinedCallsHistorySuccess','bluetooth-pbap','history', {list="cch"})
+_AFT.testVerbStatusSuccess('testSearchSuccess','bluetooth-pbap','search', {number="100"})
+_AFT.testVerbStatusSuccess('testStatusSuccess','bluetooth-pbap','status', {value="connected"})
+_AFT.testVerbStatusSuccess('testSubscribeStatusSuccess','bluetooth-pbap','subscribe', {value="status"})
+_AFT.testVerbStatusSuccess('testUnsubscribeStatusSuccess','bluetooth-pbap','unsubscribe', {value="status"})
