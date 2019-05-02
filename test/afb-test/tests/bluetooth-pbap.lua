961d3a2a245641f272758a17feffc21ff7c0e39a
Edi Feschiyan <edi.feschiyan@konsulko.com>
2019-05-02 15:28:32 +0300

    Initial commit to push in Gitlab

---
+++ test/afb-test/tests/bluetooth-pbap.lua
@@ -0,0 +1,26 @@
+--[[
+ Copyright 2019 Konsulko Group
+
+ author:Edi Feschiyan <edi.feschiyan@konsulko.com>
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+--]]
+
+
+_AFT.testVerbStatusSuccess('testcontactsSuccess','bluetooth-pbap','contacts', {})
+_AFT.testVerbStatusSuccess('testentrySuccess','bluetooth-pbap','entry', {})
+_AFT.testVerbStatusSuccess('testhistorySuccess','bluetooth-pbap','history', {})
+_AFT.testVerbStatusSuccess('testsearchSuccess','bluetooth-pbap','search', {})
+_AFT.testVerbStatusSuccess('teststatusSuccess','bluetooth-pbap','status', {})
+_AFT.testVerbStatusSuccess('testsubscribeSuccess','bluetooth-pbap','subscribe', {})
+_AFT.testVerbStatusSuccess('testunsubscribeSuccess','bluetooth-pbap','unsubscribe', {})
